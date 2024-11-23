fe7fc1d70448819998694de97db783b92e936987
catarinaperei <smlilwana@gmail.com>
2024-11-23 14:32:05 +0200

    commit Initial

---
+++ Factorial.py
@@ -0,0 +1,14 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+a=int(input("enter a number:"))
+b=int(input("enter b number:"))
+print(add(a,b))
+print(sub(a,b))
+print(mul(a,b))
+print(div(a,b))
---
+++ Indroduction.py
@@ -0,0 +1,4 @@
+name=input("enter your name")
+def intro(name):
+    print("my name is",name)
+intro(name)   
